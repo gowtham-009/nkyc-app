72b8cad6eda0ee8cab63adced12cc59c1bc8a74b
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-04-09 17:58:53 +0530

    today

---
+++ components/forminputs/dateinput.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col">
+   
+    <input
+      type="date"
+     v-model="date"
+     
+      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+    />
+  </div>
+</template>
